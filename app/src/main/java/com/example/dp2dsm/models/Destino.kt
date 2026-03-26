19a34f0e766bba1c4e32019bbece31a613ff4171
CC230286 <efraincortez777@gmail.com>
2026-03-26 12:08:17 -0600

    agregar: modelo de datos para los destinos turísticos

---
+++ app/src/main/java/com/example/dp2dsm/models/Destino.kt
@@ -0,0 +1,22 @@
+package com.example.dp2dsm.models
+
+
+
+data class Destino(
+
+    var id: String? = null,
+
+
+    val nombre: String = "",
+
+
+    val pais: String = "",
+
+    val precio: Double = 0.0,
+
+    val descripcion: String = "",
+
+    val imageUrl: String = ""
+) {
+
+}
